c191b228acc35ef2a1254d84bd2ce2ef25dabea6
Jefferson Silva <jeffsilva1@outlook.com>
2022-07-31 18:27:11 -0300

    Fix collectstatic

--- ssl_monitor/settings.py
+++ ssl_monitor/settings.py
@@ -120,6 +120,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
+# STATIC_ROOT = BASE_DIR / 'staticfiles/'
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'staticfiles'),
+)
 
 # Enable WhiteNoise's GZip compression of static assets.
 # STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
