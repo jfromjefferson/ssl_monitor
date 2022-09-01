5b58de28d76bc7e2a86cfce7f4681ae592dc721d
Jefferson Silva <jeffsilva1@outlook.com>
2022-09-01 13:00:17 -0300

    Update

--- ssl_monitor/ssl_monitor/settings.py
+++ ssl_monitor/ssl_monitor/settings.py
@@ -26,7 +26,7 @@ DEBUG = os.environ.get('DJANGO_DEBUG') == 'DEBUG'
 
 ALLOWED_HOSTS = ['localhost', 'ssl-monitor:8080', 'ssl-monitor']
 
-CSRF_TRUSTED_ORIGINS = ['http://localhost']
+CSRF_TRUSTED_ORIGINS = ['http://localhost', 'https://*.oitobits.tech']
 
 
 # Application definition
